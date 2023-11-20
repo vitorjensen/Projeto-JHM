dc1765a83128661739634283ac9e8e949f6386e7
vitorjensen <68156378+vitorjensen@users.noreply.github.com>
2023-11-20 11:52:16 -0300

    homepage_update-20-11

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -7,21 +7,22 @@
     <link rel="stylesheet" type="text/css" href="http://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.css"/>
     <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick-theme.css"/>
     <link rel="stylesheet" href="css/homepage.css">
+    <script src="js/script.js" defer></script>
     <title>JHM | QUALIDADES</title>
 </head>
 <body>
     <header>
         <nav class="nav-bar">
             <div class="logo">
-                <img src="img/jhm-logo-branco.png" alt="Logo JHM">
+                <a href="homepage.html"><img src="img/jhm-logo-branco.png" alt="Logo JHM"></a>
             </div>
 
             <div class="nav-list">
                 <ul>
                     <li class="nav-item"><a href="#" class="nav-link">EMPRESA</a></li>
-                    <li class="nav-item" id="ativo"><a href="qualidade.html" class="nav-link">QUALIDADES</a></li>
+                    <li class="nav-item"><a href="qualidades" class="nav-link">QUALIDADES</a></li>
                     <li class="nav-item"><a href="#" class="nav-link">PRODUTOS</a></li>
-                    <li class="nav-item"><a href="contato.html" class="nav-link">CONTATO</a></li>
+                    <li class="nav-item"><a href="contato" class="nav-link">CONTATO</a></li>
                 </ul>
             </div>
 
@@ -47,25 +48,67 @@
                 <li class="nav-item"><a href="#" class="nav-link">EMPRESA</a></li>
                 <li class="nav-item"><a href="qualidade.html" class="nav-link">QUALIDADES</a></li>
                 <li class="nav-item"><a href="#" class="nav-link">PRODUTOS</a></li>
-                <li class="nav-item"><a href="#" class="nav-link">CONTATO</a></li>
+                <li class="nav-item"><a href="contato.html" class="nav-link">CONTATO</a></li>
             </ul>
         </div>
     </header>
 
 
     <main>
-        <div class="carrossel">
-            <div><img src="img/1.png" alt="motor 1"></div>
-            <!-- <div><img src="img/2.png" alt="motor 2"></div>
-            <div><img src="img/3.png" alt="motor 3"></div> -->
-        </div>
+        <section class="container-principal">
+            <button id="prev-button"><img src="img/prev.png" alt="prev-button"></button>
+            <button id="next-button"><img src="img/next.png" alt="next-button"></button>
+            <div class="container-carrossel">
+                <div class="carrossel-images">
+                    <img src="img/homepage-grd/1.png" alt="motor 1" class="slider on">
+                    <img src="img/homepage-grd/2.png" alt="motor 2" class="slider">
+                    <img src="img/homepage-grd/3.png" alt="motor 3" class="slider">
+                </div>
+            </div>
+
+            <div class="scroll-down">
+
+            </div>
+        </section>
+
+
+        <section class="container-divs">
+            <div class="div-pqn"> </div>
+            <div class="div-gnd"> </div>
+            <div class="div-pqn"> </div>
+        </section>
+
+
 
         <div class="center">
-            <div class="item"><img class="img-carrossel-centro" src="img/motores/motor-a-prova-de-explosao.jpg" alt="motor 1"></div>
-            <div class="item"><img class="img-carrossel-centro" src="img/motores/motor-freio-a-prova-de-explosao.jpg" alt="motor 1"></div>
-            <div class="item"><img class="img-carrossel-centro" src="img/motores/motor-freio-redutor-tipo-1.jpg" alt="motor 1"></div>
-            <div class="item"><img class="img-carrossel-centro" src="img/motores/w22-premium.jpg" alt="motor 1"></div>
-
+            <div class="item">
+                <div class="desc-motor">
+                    <h2>MOTOR À PROVA DE EXPLOSÃO</h2>
+                    <p>Proteção IP55</p>
+                </div>
+                <img class="img-carrossel-centro" src="img/motores/motor-a-prova-de-explosao.jpg" alt="motor 1">
+            </div>
+            <div class="item">
+                <div class="desc-motor">
+                    <h2>MOTOR À PROVA DE EXPLOSÃO</h2>
+                    <p>Proteção IP66W</p>
+                </div>
+                <img class="img-carrossel-centro" src="img/motores/motor-freio-a-prova-de-explosao.jpg" alt="motor 1">
+            </div>
+            <div class="item">
+                <div class="desc-motor">
+                    <h2>MOTO FREIO</h2>
+                    <p>Redutor tipo 1</p>
+                </div>
+                <img class="img-carrossel-centro" src="img/motores/motor-freio-redutor-tipo-1.jpg" alt="motor 1">
+            </div>
+            <div class="item">
+                <div class="desc-motor">
+                    <h2>W22 Premium</h2>
+                    <p>Potência: 0,16 a 750cv</p>
+                </div>
+                <img class="img-carrossel-centro" src="img/motores/w22-premium.jpg" alt="motor 1">
+            </div>
         </div>
     </main>
 
@@ -86,10 +129,7 @@
             <p class="direitos-p">Todos os Direitos Reservados  JHM Motores - Desenvolvido por alunos da <a href="">FUMEP, EEP</a></p>
         </div>
     </footer>
-
-    <script src="js/script.js"></script>
     
-
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.4/jquery.min.js"></script>
     <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.js"></script>
 
