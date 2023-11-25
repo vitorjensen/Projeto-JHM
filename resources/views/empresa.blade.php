735207ca0456c972ac65f24fa52a1267cf6b1089
Vitor Jensen <vitorjensen6@gmail.com>
2023-11-24 23:36:44 -0300

    update_empresa+btmGoogle-24-11-23

--- resources/views/empresa.blade.php
+++ resources/views/empresa.blade.php
@@ -5,8 +5,29 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="shortcut icon" href="img/jhm-icon.ico" type="image/x-icon">
     <link rel="stylesheet" href="css/empresa.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <title>JHM | EMPRESA</title>
 </head>
+<style>
+.dropdown {
+  position: relative;
+  display: inline-block;
+}
+
+.dropdown-content {
+  display: none;
+  position: absolute;
+  background-color: #f9f9f9;
+  min-width: 160px;
+  box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.2);
+  padding: 12px 20px;
+  z-index: 1;
+}
+
+.dropdown:hover .dropdown-content {
+  display: block;
+}
+</style>
 <body>
     <header>
         <nav class="nav-bar">
@@ -32,7 +53,24 @@
             </label>
 
             <div class="nav-profile">
-                <a href="minhaconta"><img src="img/perfil.png" alt="Perfil"></a>
+            <div class="dropdown">
+            <a href="minhaconta"><img src="img/perfil.png" alt="Perfil"></a>
+             @if(auth()->check())
+            <div class="dropdown-content">
+            <h5 class="dropdown-item dropdown-footer">
+          <a href="#" class="dropdown-item dropdown-footer">
+          <i class="fa fa-gear" style="font-size:18px"></i> {{ __('Perfil') }}</a>
+          </a>
+          </h5>
+        
+          <br>
+          <h5 class="dropdown-item dropdown-footer">
+          <a href="{{route('route.destroy')}}" class="dropdown-item dropdown-footer">
+          <i class="fa fa-sign-out" style="font-size:18px"></i> {{ __('Sair') }}</a>
+          </a>
+          </h5>
+         </div>
+                @endif
             </div>
         </nav>
 
