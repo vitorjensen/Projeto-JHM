a0e44fc595e62ff0c1e02d88433844a893210df2
vitorjensen <68156378+vitorjensen@users.noreply.github.com>
2023-12-27 14:51:46 -0300

    extends-27-12-23

--- resources/views/_layouts/auth_index.blade.php
+++ resources/views/_layouts/auth_index.blade.php
@@ -5,10 +5,10 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     
     <link rel="shortcut icon" href="img/jhm-icon.ico" type="image/x-icon">
-    <link rel="stylesheet" href="css/reset.css">
-    <link rel="stylesheet" href="css/nav.css">
-    <link rel="stylesheet" href="css/iniciar-sessao.css">
-    <link rel="stylesheet" href="css/footer.css">
+    <link rel="stylesheet" href="{{asset('css/reset.css')}}">
+    <link rel="stylesheet" href="{{asset('css/nav.css')}}">
+    <link rel="stylesheet" href="{{asset('css/iniciar-sessao.css')}}">
+    <link rel="stylesheet" href="{{asset('css/footer.css')}}">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.css">
@@ -40,7 +40,7 @@
     <header>
         <nav class="nav-bar">
             <div class="logo">
-            <a href="/"><img src="img/jhm-logo-branco.png" alt="Logo JHM"></a>
+            <a href="/"><img src="{{asset('img/jhm-logo-branco.png')}}" alt="Logo JHM"></a>
             </div>
 
             <div class="nav-list">
@@ -63,7 +63,7 @@
 </div>
             <div class="nav-profile">
             <div class="dropdown">
-            <a href="minhaconta"><img src="img/perfil.png" alt="Perfil"></a>
+            <a href="minhaconta"><img src="{{asset('img/perfil.png')}}" alt="Perfil"></a>
              @if(auth()->check())
             <div class="dropdown-content">
             <h5 class="dropdown-item dropdown-footer">
@@ -85,7 +85,7 @@
 
         <div class="menu-mobile">
             <div class="nav-profile">
-                <a href="minhaconta"><img src="img/perfil.png" alt="Perfil"> </a>
+                <a href="minhaconta"><img src="{{asset('img/perfil.png')}}" alt="Perfil"> </a>
                 
             </div>
             
@@ -112,6 +112,8 @@
 @yield('main_minhaconta')
 @yield('main_iniciarsessao')
 @yield('main_esquecisenha')
+@yield('main_resetpassword')
+@yield('main_verifyemail')
 
     <footer>
     <section class="footer-grid">
@@ -122,7 +124,7 @@
             <p><a href="mailto:vendas@jhmmotores.com.br">vendas@jhmmotores.com.br</a></p>
         </div>
         <div class="footer-grid-img">
-            <img src="img/jhm-logo-96.png" alt="Logo JHM Motores">
+            <img src="{{asset('img/jhm-logo-96.png')}}" alt="Logo JHM Motores">
         </div>
     </section>
     <div class="direitos">
@@ -130,7 +132,7 @@
     </div>
 </footer>
 
-<script src="js/script.js"></script>
+<script src="{{asset('js/script.js')}}"></script>
  <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
