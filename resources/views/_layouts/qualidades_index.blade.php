71c0d03a77e5a0e284cfcb54c6dbb456748dccc2
vitorjensen <68156378+vitorjensen@users.noreply.github.com>
2023-12-27 16:20:59 -0300

    components-27-12-23

--- resources/views/_layouts/qualidades_index.blade.php
+++ resources/views/_layouts/qualidades_index.blade.php
@@ -29,64 +29,9 @@
 }
 </style>
 <body>
-    <header>
-        <nav class="nav-bar">
-            <div class="logo">
-              <a href="/"><img src="img/jhm-logo-branco.png" alt="Logo JHM"></a>
-            </div>
+        <x-header>
 
-            <div class="nav-list">
-                <ul>
-                    <li class="nav-item"><a href="empresa" class="nav-link">EMPRESA</a></li>
-                    <li class="nav-item" id="ativo"><a href="qualidades" class="nav-link">QUALIDADES</a></li>
-                    <li class="nav-item"><a href="produtos" class="nav-link">PRODUTOS</a></li>
-                    <li class="nav-item"><a href="{{route('route.contato')}}" class="nav-link">CONTATO</a></li>
-                </ul>
-            </div>
-
-            <input type="checkbox" name="" id="menu-hamburguer" onclick="menuAparecer()">
-
-            <label for="menu-hamburguer">
-                <div id="menu-icon">
-                    <span class="hamburguer"></span>
-                </div>
-            </label>
-
-            <div class="nav-profile">
-            <div class="dropdown">
-            <a href="minhaconta"><img src="img/perfil.png" alt="Perfil"></a>
-             @if(auth()->check())
-            <div class="dropdown-content">
-            <h5 class="dropdown-item dropdown-footer">
-          <a href="#" class="dropdown-item dropdown-footer">
-          <i class="fa fa-gear" style="font-size:18px"></i> {{ __('Perfil') }}</a>
-          </a>
-          </h5>
-        
-          <br>
-          <h5 class="dropdown-item dropdown-footer">
-          <a href="{{route('route.destroy')}}" class="dropdown-item dropdown-footer">
-          <i class="fa fa-sign-out" style="font-size:18px"></i> {{ __('Sair') }}</a>
-          </a>
-          </h5>
-         </div>
-                @endif
-            </div>
-        </nav>
-
-        <div class="menu-mobile">
-            <div class="nav-profile">
-                <a href="minhaconta"><img src="img/perfil.png" alt="Perfil"></a>
-            </div>
-            
-            <ul>
-                <li class="nav-item"><a href="empresa" class="nav-link">EMPRESA</a></li>
-                <li class="nav-item"><a href="qualidades" class="nav-link">QUALIDADES</a></li>
-                <li class="nav-item"><a href="produtos" class="nav-link">PRODUTOS</a></li>
-                <li class="nav-item"><a href="contato" class="nav-link">CONTATO</a></li>
-            </ul>
-        </div>
-    </header>
+    </x-header>
     <section class="secao-head-pagina">
         <div class="secao-linha-branca">
             <h1>QUALIDADES</h1>
